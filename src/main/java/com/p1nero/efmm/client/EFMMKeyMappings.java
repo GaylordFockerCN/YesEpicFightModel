35f12c2812e9457ee26b448b2fbdb0898a1004bb
P1nero <1070402601@qq.com>
2025-03-24 12:34:13 +0800

    换双键打开

--- src/main/java/com/p1nero/efmm/client/EFMMKeyMappings.java
+++ src/main/java/com/p1nero/efmm/client/EFMMKeyMappings.java
@@ -1,5 +1,6 @@
 package com.p1nero.efmm.client;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.p1nero.efmm.client.gui.screen.SelectEFModelScreen;
 import com.p1nero.efmm.network.PacketHandler;
 import com.p1nero.efmm.network.PacketRelay;
@@ -7,8 +8,10 @@ import com.p1nero.efmm.network.packet.RequestBindModelPacket;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.client.settings.KeyModifier;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
@@ -19,27 +22,28 @@ import java.util.function.BiConsumer;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class EFMMKeyMappings {
-    public static final KeyMapping OPEN_SELECT_SCREEN = new KeyMapping("key.efmm.open_select_screen", GLFW.GLFW_KEY_M, "key.categories.efmm");
+    public static final KeyMapping OPEN_SELECT_SCREEN = new KeyMapping("key.efmm.open_select_screen", KeyConflictContext.IN_GAME, KeyModifier.CONTROL, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_M, "key.categories.efmm");
 
     @SubscribeEvent
     public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
         event.register(OPEN_SELECT_SCREEN);
     }
 
-
     @Mod.EventBusSubscriber(value = Dist.CLIENT)
     public static class HandleKeyInput{
         @SubscribeEvent
-        public static void registerKeyMappings(InputEvent.Key event) {
-            if (Minecraft.getInstance().player != null && Minecraft.getInstance().screen == null && !Minecraft.getInstance().isPaused()) {
-                if(event.getAction() == 1 && event.getKey() == OPEN_SELECT_SCREEN.getKey().getValue()){
-                    Minecraft.getInstance().setScreen(new SelectEFModelScreen(callBack, callBack));
+        public static void onClientTick(TickEvent.ClientTickEvent event) {
+            if(event.phase.equals(TickEvent.Phase.END)){
+                while (OPEN_SELECT_SCREEN.consumeClick()){
+                    if (Minecraft.getInstance().player != null && Minecraft.getInstance().screen == null && !Minecraft.getInstance().isPaused()) {
+                        Minecraft.getInstance().setScreen(new SelectEFModelScreen(SELECT_CALL_BACK, SELECT_CALL_BACK));
+                    }
                 }
             }
         }
     }
 
-    public static BiConsumer<String, MeshProvider<AnimatedMesh>> callBack = ((modelId, meshProvider) -> {
+    private static final BiConsumer<String, MeshProvider<AnimatedMesh>> SELECT_CALL_BACK = ((modelId, meshProvider) -> {
         if(!modelId.isEmpty()){
             PacketRelay.sendToServer(PacketHandler.MAIN_CHANNEL, new RequestBindModelPacket(modelId));
         }
